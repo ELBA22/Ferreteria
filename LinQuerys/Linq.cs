0e1c0574b0e9162d8576d1a63980f2cbd45997b1
Elba Garcia <egarcias2021.net@gmail.com>
2023-11-03 11:27:14 -0500

    feat: :sparkles: Funciones y metodos

--- LinQuerys/Linq.cs
+++ LinQuerys/Linq.cs
@@ -26,10 +26,10 @@ namespace Ferreteria.LinQuerys
 
         List<Factura> FacturaList = new List<Factura>()
         {
-            new Factura(){NumeroFactura = 1, Fecha = new DateTime (12-05-2023), IdCliente = 01, TotalFactura = 30000 },
-            new Factura(){NumeroFactura = 2, Fecha = new DateTime (25-04-2023), IdCliente = 02, TotalFactura = 40000 },
-            new Factura(){NumeroFactura = 3, Fecha = new DateTime (29-01-2023), IdCliente = 03, TotalFactura = 8000 },
-            new Factura(){NumeroFactura = 4, Fecha = new DateTime (01-01-2023), IdCliente = 04, TotalFactura = 15000}
+            new Factura(){NumeroFactura = 1, Fecha = new DateTime (2023, 10, 12), IdCliente = 01, TotalFactura = 30000 },
+            new Factura(){NumeroFactura = 2, Fecha = new DateTime (2023, 05, 22), IdCliente = 02, TotalFactura = 40000 },
+            new Factura(){NumeroFactura = 3, Fecha = new DateTime (2023,01,11), IdCliente = 03, TotalFactura = 8000 },
+            new Factura(){NumeroFactura = 4, Fecha = new DateTime (2023, 01,  12), IdCliente = 04, TotalFactura = 15000}
         };
 
         List<DetalleFactura> DetalleFacturaList = new List<DetalleFactura>()
@@ -42,90 +42,138 @@ namespace Ferreteria.LinQuerys
 
         public void MainMenu()
         {
-            Console.WriteLine("      INVENTARIO FERRETERIA CAMPUSLAND          ");
-            Console.WriteLine("       Ingrese la opcion a realizar            ");
-            Console.WriteLine("1.    Listar productos de inventario         ");
-            Console.WriteLine("2.  Productos que estan a punto de agotarse");
-            Console.WriteLine("3.        Productos a comprar");
-            Console.WriteLine("4. Total de facturas del mes de enero del 2023");
-            Console.WriteLine("5.   Productos vendidos de una factura");
-            Console.WriteLine("6.     Valor total del inventario");
-            string opcion = Console.ReadLine();
-
-            switch(opcion)
+            bool controlador = true;
+            while (controlador)
             {
-                case "1":
-                ProductosAListar();
-                break;
+                Console.Clear();
+                Console.WriteLine("------------INVENTARIO FERRETERIA CAMPUSLAND------------");
+                Console.WriteLine("|              INGRESE LA OPCION A REALIZAR.            |");
+                Console.WriteLine("|1.              productos de inventario                |");
+                Console.WriteLine("|2.       Productos que estan a punto de agotarse       |");
+                Console.WriteLine("|3.                Productos a comprar                  |");
+                Console.WriteLine("|4.      Total de facturas del mes de enero del 2023    |");
+                Console.WriteLine("|5.           Productos vendidos de una factura         |");
+                Console.WriteLine("|6.               Valor total del inventario            |");
+                Console.WriteLine("|7------                    Salir              ---------|");
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        ProductosAListar();
+                        Console.ReadLine();
+                        break;
 
-                case "2":
-                ListarAgotados();
-                break;
+                    case "2":
+                        ListarAgotados();
+                        Console.ReadLine();
+                        break;
 
-                case "3":
-                break;
+                    case "3":
+                        ListProComprar();
+                        Console.ReadLine();
+                        break;
 
-                case "4":
-                ListFacturaEnero();
-                break;
+                    case "4":
+                        ListFacturaEnero();
+                        Console.ReadLine();
+                        break;
 
-                case "5":
-                break;
+                    case "5":
+                        ProductosVendidos();
+                        Console.ReadLine();
+                        break;
 
-                case "6":
-                break;
+                    case "6":
+                        ValorTotalInventario();
+                        Console.ReadLine();
+                        break;
 
-                default:
-                    Console.WriteLine("Opcion no valida, presione una tecla para continuar...");
-                    Console.ReadKey();
-                    break;
+                    case "7":
+                        controlador = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion no valida, presione una tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                }
+
             }
         }
 
-//Opcion 1, listar productos.
-        public void ProductosAListar(){
+        //Opcion 1, listar productos.
+        public void ProductosAListar()
+        {
             var ProductosInventario = from i in ProductosList
-            select i;
-                foreach( var producto in ProductosInventario)
-                {
-                    Console.WriteLine(producto.Nombre);
-                }
+                                    select i;
+            foreach (var producto in ProductosInventario)
+            {
+                Console.WriteLine($"Id {producto.IdProducto}, Nombre {producto.Nombre}, Cantidad {producto.Cantidad}");
+            }
         }
 
-//Opcion 2, Listar productos agotados.
-        public void ListarAgotados(){
+        //Opcion 2, Listar productos agotados.
+        public void ListarAgotados()
+        {
             var ProductosAgotados = from i in ProductosList
-            where i.Cantidad< i.StockMinimo
-            select i;
-                foreach(var producto in ProductosAgotados)
-                {
-                    Console.WriteLine(producto.Nombre);
-                }
+                                    where i.Cantidad < i.StockMinimo
+                                    select i;
+            foreach (var producto in ProductosAgotados)
+            {
+                Console.WriteLine($" Nombre  {producto.Nombre}");
+            }
         }
 
 
-//Opcion 3, Listar productos que se deben comprar.
-        public void ListProComprar(){
-            var ProductosComprar =  ProductosList.Where(Productos => Productos.Cantidad<Productos.StockMaximo);
-            foreach( var producto in ProductosComprar)
+        //Opcion 3, Listar productos que se deben comprar.
+        public void ListProComprar()
+        {
+            var ProductosComprar = ProductosList.Where(Productos => Productos.Cantidad < Productos.StockMaximo);
+            foreach (var producto in ProductosComprar)
             {
                 int cantidadComprar = producto.StockMaximo - producto.Cantidad;
-                Console.WriteLine();
+                Console.WriteLine($"ID: {producto.IdProducto}, Nombre: {producto.Nombre}, Cantidad a Comprar: {producto.Cantidad}" );
             }
         }
 
-//Opcion 4, facturas de enero.
-        public void ListFacturaEnero(){
-            var FacturasEnero = from i in FacturaList
-            where i.Fecha.Year == 2023 && i.Fecha.Month == 01
-            select i;
-            foreach( var producto in FacturasEnero)
+        //Opcion 4, facturas de enero.
+        public void ListFacturaEnero()
+        {
+                    var FacturasEnero = from i in FacturaList
+                                            where i.Fecha.Year == 2023 && i.Fecha.Month == 1
+                                            select i;
+                        foreach (var productos in FacturasEnero)
+                        {
+                            Console.WriteLine($"Valor facturas del mes de enero: {productos.TotalFactura }");
+                        } 
+        }
+
+        //Opcion 5, Listar productos de una factura determinada.
+        public void ProductosVendidos()
+        {
+            Console.WriteLine("Ingrese el numero de factura que desea consultar : ");
+            int numeroFactura = Int32.Parse(Console.ReadLine());
+            var productos = from pro in ProductosList
+                            join detfac in DetalleFacturaList
+                            on pro.IdProducto equals detfac.IdProducto
+                            select new
+                            {
+                                producto = pro.Nombre,
+                                id = pro.IdProducto
+                            };
+            foreach (var i in productos)
             {
-                Console.WriteLine(producto.NumeroFactura);
+                Console.WriteLine($"Id {i.id}, Nombre: {i.producto}");
             }
         }
 
-//Opcion 5, Listar productos de una factura determinada
-        
+        //opcion 6,  valor total de inventario
+        public void ValorTotalInventario(){
+            var valortotal = from i in ProductosList
+            select i.PrecioUnitario * i.Cantidad;
+            double Resultadototal = valortotal.Sum();
+            Console.WriteLine("El valor total del inventario es: " + Resultadototal);
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using Ferreteria.LinQuerys;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Linq FerreteriaLinq = new Linq();
+        FerreteriaLinq.MainMenu();
     }
 }
